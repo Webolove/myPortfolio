d4e5a97035561c48d59ab914cc1067997097b48e
MUDIT ANAND <102852265+Webolove@users.noreply.github.com>
2023-06-26 09:44:28 +0530

    Update dayNight

--- public/Js/dayNight.js
+++ public/Js/dayNight.js
@@ -1,46 +1,45 @@
 let timeZone = document.getElementById("day_night");
 let dayTheme = document.getElementById("DayTheme");
 let nightTheme = document.getElementById("NightTheme");
-
+var theme;
 const dayAudio = document.getElementById("dayAudio");
-
-timeZone.addEventListener("click",()=>{
-    if(timeZone.classList.contains("uil-sun"))
-    {
-        timeZone.classList.add("active_rotate");
-        dayAudio.play();
-        setTimeout(()=>{
-            timeZone.classList.remove("uil-sun");
-            timeZone.classList.add("uil-moon");
-
-            dayTheme.removeAttribute("disabled");
-            nightTheme.setAttribute("disabled", "true");
-
-            timeZone.classList.remove("active_rotate");
-        }, 300);
-    }
-    else{
-        timeZone.classList.add("active_rotate");
-        dayAudio.play();
-        setTimeout(()=>{
-            timeZone.classList.add("uil-sun");
-            timeZone.classList.remove("uil-moon");
-
-            nightTheme.removeAttribute("disabled");
-            dayTheme.setAttribute("disabled", "true");
-
-            timeZone.classList.remove("active_rotate");
-        }, 300);
-    }
-})
 
 
 let getTheme = JSON.parse(localStorage.getItem('PageTheme'));
-console.log(getTheme)
 if (getTheme === "Night") {
-    console.log('hi')
     timeZone.classList.add("uil-sun");
     timeZone.classList.remove("uil-moon");
     nightTheme.removeAttribute("disabled");
     dayTheme.setAttribute("disabled", "true");
 }
+timeZone.addEventListener("click", () => {
+    if (timeZone.classList.contains("uil-sun")) {
+        timeZone.classList.add("active_rotate");
+        dayAudio.play();
+        setTimeout(() => {
+            timeZone.classList.remove("uil-sun");
+            timeZone.classList.add("uil-moon");
+
+            dayTheme.removeAttribute("disabled");
+            nightTheme.setAttribute("disabled", "true");
+            theme = "Day";
+            timeZone.classList.remove("active_rotate");
+            localStorage.setItem('PageTheme', JSON.stringify(theme));
+        }, 300);
+    }
+    else {
+        timeZone.classList.add("active_rotate");
+        dayAudio.play();
+        setTimeout(() => {
+            timeZone.classList.add("uil-sun");
+            timeZone.classList.remove("uil-moon");
+
+            nightTheme.removeAttribute("disabled");
+            dayTheme.setAttribute("disabled", "true");
+            theme = "Night";
+            timeZone.classList.remove("active_rotate");
+            localStorage.setItem('PageTheme', JSON.stringify(theme));
+        }, 300);
+    }
+    
+})
